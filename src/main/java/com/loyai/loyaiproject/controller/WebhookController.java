ffa97b97997b9b14a2a755edb29a073ecf5fc9a0
Chris Ogwuche <ogwuchechris@gmail.com>
2023-10-18 16:08:59 +0100

    updated

--- src/main/java/com/loyai/loyaiproject/controller/WebhookController.java
+++ src/main/java/com/loyai/loyaiproject/controller/WebhookController.java
@@ -4,14 +4,12 @@ import com.loyai.loyaiproject.service.WebhookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/webhook/game")
+@CrossOrigin("*")
 @Slf4j
 public class WebhookController {
     private final WebhookService webhookService;
