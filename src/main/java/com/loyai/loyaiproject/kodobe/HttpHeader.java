a44d05df2ede9a22bfd547af06747857a3623dc1
Chris Ogwuche <ogwuchechris@gmail.com>
2024-02-19 08:50:15 +0100

    refactored HttpHeader class

--- src/main/java/com/loyai/loyaiproject/kodobe/HttpHeader.java
+++ src/main/java/com/loyai/loyaiproject/kodobe/HttpHeader.java
@@ -3,6 +3,7 @@ package com.loyai.loyaiproject.kodobe;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @Data
 @NoArgsConstructor
@@ -12,15 +13,16 @@ public class HttpHeader {
 
     public HttpHeader(String clientId,String secret){
         headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("client-id",clientId);
         headers.set("secret",secret);
     }
 
     public HttpHeader(String clientId,String secret,String bearerToken){
         headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("client-id",clientId);
         headers.set("secret",secret);
         headers.set("Authorization", "Bearer " +bearerToken);
     }
-
 }
